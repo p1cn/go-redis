15ba0a21fd1d7f6bf7ef3725166c4962c4c86e24
liyunlong <liyunlong@p1.com>
2025-04-14 11:08:27 +0800

    log

--- cluster.go
+++ cluster.go
@@ -743,7 +743,7 @@ type ClusterClient struct {
 // http://redis.io/topics/cluster-spec.
 func NewClusterClient(opt *ClusterOptions) *ClusterClient {
 	opt.init()
-
+	fmt.Println("NewClusterClient Syncing delay 10s")
 	c := &ClusterClient{
 		clusterClient: &clusterClient{
 			opt:   opt,
